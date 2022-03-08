b105b02d829e399a1229b6f24e2af1c9806f7e1b
Spicebag <94927382+spicebag@users.noreply.github.com>
2022-03-08 12:29:21 +0000

    Update Settings.Designer.cs

--- Source/Cryptic Stresser/Cryptic_Stresser/Properties/Settings.Designer.cs
+++ Source/Cryptic Stresser/Cryptic_Stresser/Properties/Settings.Designer.cs
@@ -5,13 +5,10 @@ using System.Runtime.CompilerServices;
 
 namespace Cryptic_Stresser.Properties
 {
-	// Token: 0x02000015 RID: 21
 	[GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
 	[CompilerGenerated]
 	internal sealed partial class Settings : ApplicationSettingsBase
 	{
-		// Token: 0x17000005 RID: 5
-		// (get) Token: 0x0600007B RID: 123 RVA: 0x0000ECAC File Offset: 0x0000CEAC
 		public static Settings Default
 		{
 			get
@@ -20,7 +17,6 @@ namespace Cryptic_Stresser.Properties
 			}
 		}
 
-		// Token: 0x040000BD RID: 189
 		private static Settings defaultInstance = (Settings)SettingsBase.Synchronized(new Settings());
 	}
 }
