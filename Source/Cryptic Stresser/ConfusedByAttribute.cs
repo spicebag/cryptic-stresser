f8336a527baed47d5f7f7c8647dc5456ad384848
Spicebag <94927382+spicebag@users.noreply.github.com>
2022-03-08 12:28:07 +0000

    Update ConfusedByAttribute.cs

--- Source/Cryptic Stresser/ConfusedByAttribute.cs
+++ Source/Cryptic Stresser/ConfusedByAttribute.cs
@@ -1,9 +1,7 @@
-﻿using System;
+using System;
 
-// Token: 0x0200002C RID: 44
 internal class ConfusedByAttribute : Attribute
 {
-	// Token: 0x0600010D RID: 269 RVA: 0x00002652 File Offset: 0x00000852
 	public ConfusedByAttribute(string A_1)
 	{
 	}
